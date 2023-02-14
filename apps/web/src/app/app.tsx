a50fabf218c2da107ac9667713b0e4602c702b36
vleonardoce <vcorderoespinoza@gmail.com>
2023-02-13 20:19:14 -0500

    tailwind implementation

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,51 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import Header from './layout/header/header';
+import Footer from './layout/footer/footer';
+import Main from './main/main';
+import { Route, Routes } from 'react-router-dom';
+import Store from './store/store';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="web" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+      <Header></Header>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<Main />} />
+        <Route path="/tienda" element={<Store />} />
       </Routes>
-      {/* END: routes */}
+      <Footer></Footer>
     </>
   );
 }
---
+++ apps/web/src/app/main/main.tsx
@@ -0,0 +1,13 @@
+import Hero from './hero/hero';
+import './main.scss';
+
+/* eslint-disable-next-line */
+export interface MainProps {}
+
+export function Main(props: MainProps) {
+  return (
+    <Hero></Hero>
+  );
+}
+
+export default Main;
