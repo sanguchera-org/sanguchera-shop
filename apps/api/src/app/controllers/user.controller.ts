206b4b6682b6adbf25ad7c83259ac7487768aab4
vleonardoce <vcorderoespinoza@gmail.com>
2023-03-15 19:45:33 -0500

    [feat:api] - store user with auth uid

--- apps/api/src/app/controllers/user.controller.ts
+++ apps/api/src/app/controllers/user.controller.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from "express";
 import { Firebase } from "../config/Firebase"
-import { User } from "../models/user";
 
 export const createUser = async (req: Request, res: Response) => {
     const user = req.body;
     try {
         const firestore = new Firebase().firestore()
-        const docRef = await firestore.collection("users").doc();
+        const docRef = firestore.collection("users").doc(user.uid);
         await docRef.set(user);
         res.status(201).json(docRef);
     } catch (error) {
